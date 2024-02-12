2faee234c6cd47705add57d893f77eeb00782dd3
banana <banana@gmail.com>
2024-02-12 14:01:15 +0900

    fixed the keys

--- install-ros.sh
+++ install-ros.sh
@@ -15,7 +15,11 @@ sudo apt autoclean -y
 
 #ROS stuffs
 #Set up keys
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update -y
+
 
 #Install Desktop-Full Install
 sudo apt install -y ros-melodic-desktop-full 
